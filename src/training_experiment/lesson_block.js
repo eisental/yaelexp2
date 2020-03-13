c0e87bb3c581aaf8df910aefab56f017ee33543e
eisental <eisental@gmail.com>
2020-03-13 10:19:56 +0200

    lessons starts with trial 15

--- src/training_experiment/lesson_block.js
+++ src/training_experiment/lesson_block.js
@@ -77,7 +77,7 @@ const SongWithoutChords = props => {
 // between them is the audio sequence. Audio playing code is here.
 class LessonPart extends React.Component {
   state = {
-    trial_idx: 15,
+    trial_idx: 0,
     done_playing: false,
   };
 
